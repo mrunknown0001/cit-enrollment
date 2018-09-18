b8f8c77b20e9227f4ba0d2a175bd15e8f24b84c0
Michael Adam <madamt0001@gmail.com>
2018-09-18 13:47:39 +0800

    Added

---
+++ resources/views/includes/modal-terms-and-condition.blade.php
@@ -0,0 +1,20 @@
+<div class="modal fade" id="terms" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <strong>Terms and Condition</strong>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+
+
+
+      </div>
+      <div class="modal-footer">
+        
+      </div>
+    </div>
+  </div>
+</div>
---
+++ resources/views/terms-and-condition.blade.php
@@ -0,0 +1,32 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Admin Login - Online Enrollment for CIT Colleges of Paniqui Foundation Inc.</title>
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <link rel="stylesheet"  href="{{ asset('adminlte/css/AdminLTE.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('adminlte/css/skins/skin-blue-light.min.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+  <script src="{{ asset('js/app.js') }}"></script>
+  <style type="text/css">
+    .vertical-center {
+      margin-top: 100px !important;
+    }
+  </style>
+</head>
+<body>
+<div class="row">
+  <div class="col-md-12">
+    <h3>Terms and Conditions</h3>
+  </div>
+</div>
+</body>
+</html>
