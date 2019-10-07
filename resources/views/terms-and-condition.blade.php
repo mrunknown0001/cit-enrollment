ff433b1951a7a5acc5449ad04d107cff5436518b
Michael Adam <madamt0001@gmail.com>
2019-10-07 23:34:38 +0800

    update

--- resources/views/terms-and-condition.blade.php
+++ resources/views/terms-and-condition.blade.php
@@ -29,16 +29,16 @@
 
       <h3>Privacy Statement</h3>
       <p class="italized">Last updated: September 29, 2018</p>
-      <p>Please read the Privacy Statement carefully before using the https://cit-paniqui.com/ website (the "Service") operated by Online Enrollment for CIT Colleges of Paniqui, inc. ("us", "we", or "our").</p>
+      <p>Please read the Privacy Statement carefully before using the https://cit-paniqui.com/ website (the "Service") operated by Online Enrollment for Mayantoc High School ("us", "we", or "our").</p>
       <p>Your access to and use of the Service is conditioned on your acceptance of and compliance with these Terms. These Terms apply to all visitors, users and others who access or use the Service.</p>
-      <p>By accessing or using the Service you agree to be bound by these Terms. If you disagree with any part of the terms then you may not access the Service. This Terms and Conditions agreement for Online Enrollment for CIT Colleges of Paniqui, inc. is managed by TermsFeed Terms and Conditions Generator.</p>
+      <p>By accessing or using the Service you agree to be bound by these Terms. If you disagree with any part of the terms then you may not access the Service. This Terms and Conditions agreement for Online Enrollment for Mayantoc High School is managed by TermsFeed Terms and Conditions Generator.</p>
       <h4>Accounts</h4>
       <p>When you create an account with us, you must provide us information that is accurate, complete, and current at all times. Failure to do so constitutes a breach of the Terms, which may result in immediate termination of your account on our Service.</p>
       <p>You are responsible for safeguarding the password that you use to access the Service and for any activities or actions under your password, whether your password is with our Service or a third-party service.</p>
       <p>You agree not to disclose your password to any third party. You must notify us immediately upon becoming aware of any breach of security or unauthorized use of your account.</p>
       <h4>Links To Other Web Sites</h4>
-      <p>Our Service may contain links to third-party web sites or services that are not owned or controlled by Online Enrollment for CIT Colleges of Paniqui, inc..</p>
-      <p>Online Enrollment for CIT Colleges of Paniqui, inc. has no control over, and assumes no responsibility for, the content, privacy policies, or practices of any third party web sites or services. You further acknowledge and agree that Online Enrollment for CIT Colleges of Paniqui, inc. shall not be responsible or liable, directly or indirectly, for any damage or loss caused or alleged to be caused by or in connection with use of or reliance on any such content, goods or services available on or through any such web sites or services.</p>
+      <p>Our Service may contain links to third-party web sites or services that are not owned or controlled by Online Enrollment for Mayantoc High School.</p>
+      <p>Online Enrollment for Mayantoc High School has no control over, and assumes no responsibility for, the content, privacy policies, or practices of any third party web sites or services. You further acknowledge and agree that Online Enrollment for Mayantoc High School shall not be responsible or liable, directly or indirectly, for any damage or loss caused or alleged to be caused by or in connection with use of or reliance on any such content, goods or services available on or through any such web sites or services.</p>
       <p>We strongly advise you to read the terms and conditions and privacy policies of any third-party web sites or services that you visit.</p>
       <h4>Governing Law</h4>
       <p>These Terms shall be governed and construed in accordance with the laws of Philippines, without regard to its conflict of law provisions.</p>
