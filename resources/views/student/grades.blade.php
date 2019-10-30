d9dca4bd51261d37523f9208a2753fe9632f4188
Michael Adam <madamt0001@gmail.com>
2019-10-30 11:09:48 +0800

    Update

--- resources/views/student/grades.blade.php
+++ resources/views/student/grades.blade.php
@@ -16,7 +16,7 @@
 			<div class="col-md-12">
 				@include('includes.all')
 
-				@if(count($grades) > 0)
+				@if($grades) != NULL)
 					<p><i>Available Grade for the Previous Semester You Enrolled.</i></p>
 					{{--<form>
 						<div class="form-group">
