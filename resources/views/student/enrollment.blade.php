4e1918de4b32811fc757c76ed4c731e1026991fe
Michael Adam <madamt0001@gmail.com>
2019-10-31 08:40:54 +0800

    update

--- resources/views/student/enrollment.blade.php
+++ resources/views/student/enrollment.blade.php
@@ -17,7 +17,7 @@
 				@include('includes.all')
 				@if($es->active == 1)
 					@if(empty($rp)) 
-						@include('student.includes.registration-payment')
+						{{-- @include('student.includes.registration-payment') --}}
 						{{--<p><button class="btn btn-danger" onclick="window.print()"><i class="fa fa-print"></i>  Print Pre Assessment</button></p>--}}
 						<p><button class="btn btn-danger" onclick="printAreaDiv()"><i class="fa fa-print"></i>  Print Pre Assessment</button></p>
 						
