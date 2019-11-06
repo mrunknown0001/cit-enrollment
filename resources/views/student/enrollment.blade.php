90274214d82922cb0ea4f4b20f9033886ecfcc9c
Michael Adam <madamt0001@gmail.com>
2019-11-06 21:44:57 +0800

    Update

--- resources/views/student/enrollment.blade.php
+++ resources/views/student/enrollment.blade.php
@@ -53,7 +53,7 @@
 											<tr>
 												<th class="text-center">Subjects</th>
 												<th class="text-center">Days</th>
-												<th class="text-center">Units</th>
+												{{-- <th class="text-center">Units</th> --}}
 												<th class="text-center">Room</th>
 												<th class="text-center">Time</th>
 											</tr>
@@ -81,7 +81,7 @@
 														@endif
 													@endforeach
 												</td>
-												<td class="text-center">Lec: {{ $s->units }} {{ $s->lab_units ? '| Lab: ' . $s->lab_units : '' }}</td>
+												{{-- <td class="text-center">Lec: {{ $s->units }} {{ $s->lab_units ? '| Lab: ' . $s->lab_units : '' }}</td> --}}
 												<td class="text-center">
 													@foreach($schedules as $sched)
 														@if($sched->subject_id == $s->id)
