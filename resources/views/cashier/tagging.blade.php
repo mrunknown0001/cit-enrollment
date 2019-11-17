03f602550028f8935ac2d4dec18b3634995afef2
Michael Adam <madamt0001@gmail.com>
2019-11-17 19:53:49 +0800

    Update

--- resources/views/cashier/tagging.blade.php
+++ resources/views/cashier/tagging.blade.php
@@ -13,18 +13,17 @@
 	</section>
 	<section class="content">
 		<div class="row">
-			<div class="row">
-				<div class="col-md-4">
-					<form action="{{ route('cashier.search.student') }}" method="get" class="" autocomplete="off">
-						<div class="input-group">
-							<input type="text" name="q" class="form-control" placeholder="Search Student Name or Student ID">
-							<span class="input-group-btn">
-								<button type="submit" id="search-btn" class="btn btn-flat btn-danger"><i class="fa fa-search"></i>
-							</button>
-							</span>
-						</div>
-					</form>	
-				</div>
+			<div class="col-md-4">
+				<form action="{{ route('cashier.search.student') }}" method="get" class="" autocomplete="off">
+					<div class="input-group">
+						<input type="text" name="q" class="form-control" placeholder="Search Student Name or Student ID">
+						<span class="input-group-btn">
+							<button type="submit" id="search-btn" class="btn btn-flat btn-danger"><i class="fa fa-search"></i>
+						</button>
+						</span>
+					</div>
+				</form>	
+			</div>
 			<div class="col-md-12">
 				<h3 class="text-center"><em>Search Student to Take Payment</em></h3>
 			</div>
